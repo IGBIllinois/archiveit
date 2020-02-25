b8bddce41f2330f39135c5b102079d8469276174
David Slater <dslater@illinois.edu>
2020-02-25 12:35:52 -0600

    Added pbzip2 support

--- bin/autotar.pl
+++ bin/autotar.pl
@@ -9,6 +9,8 @@ my $sourcedir;
 my $destdir;
 my $group;
 my $dryrun = 0;
+my $processors = 1;
+my $memory = 100;
 
 sub timestamp() {
         my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
@@ -22,11 +24,13 @@ sub archive {
         my $destfile=$source;
         $destfile=~ s/_ready$//;
         $destfile="$destfile.tar.bz2";
-        my $cmd = "tar -cjvf $dest/$destfile $source |sed 's/\\/\$//' >$source.tartest.txt";
+	my $cmd = "tar -cv $source 2> $source.tartest.txt | pbzip2 -c -p$processors -m$memory > $dest/$destfile";
         print timestamp() . " Command: $cmd\n";
         if (!$dryrun) {
                 system($cmd);
+		system("sed -i 's/\\/\$//g' $source.tartest.txt");
                 system("find $source -name '*' >$source.findtest.txt");
+
                 unless(-s "$source.findtest.txt" and -s "$source.tartest.txt") {
                         die timestamp() . " $source.findtest.txt or $source.tartest.txt does not exist or has a zero size\n";
                 }
@@ -46,6 +50,8 @@ sub help() {
         print "--sourcedir              Source Directory\n";
         print "--destdir                Destination Directory\n";
         print "--group                  Group to set archive files to\n";
+	print "-p,--processors		Number of Processors (Default 1)\n";
+	print "-m,--memory		Amount of memory to use in MB (Default 100MB)\n";
         print "--dry-run                Output commands only\n";
         print "-h,--help                        This help\n";
         print "Source Directories must have _ready to be archived\n";
@@ -55,11 +61,13 @@ sub help() {
 
 
 GetOptions(
-	"h|help"	=> sub { help() },
-	"sourcedir=s"	=> \$sourcedir,
-	"destdir=s"	=> \$destdir,
-	"group=s"	=> \$group,
-	"dry-run"	=> \$dryrun,
+	"h|help"		=> sub { help() },
+	"sourcedir=s"		=> \$sourcedir,
+	"destdir=s"		=> \$destdir,
+	"group=s"		=> \$group,
+	"p|processors=i"	=> \$processors,
+	"m|memory=i"		=> \$memory,
+	"dry-run"		=> \$dryrun,
 );
 
 unless (defined $sourcedir) {
@@ -72,6 +80,7 @@ unless (defined $destdir) {
 unless (defined $group) {
 	die "Must specify group\n";
 }
+
 
 if(-e $lockfile) {
 	die "Autotar appears to be already running or a stale lockfile is present in $lockfile\n";
--- sample/slurm_autotar.sh
+++ sample/slurm_autotar.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 # ----------------SLURM Parameters----------------
 #SBATCH -p normal
-#SBATCH -n 1
-#SBATCH --mem=4g
+#SBATCH -n 4
+#SBATCH --mem=4000m
 #SBATCH -N 1
 #SBATCH -J autotar
 #SBATCH -D ~
 # ----------------Load Modules--------------------
 module load pbzip2
 # ----------------Commands------------------------
-autotar --sourcedir --destdir --group 
+
+SOURCEDIR=
+DESTDIR=
+GROUP=
+
+#Calculate 90% of reserved memory.  Use this for maximum amount of memory for pbzip2
+MEMORY=$(($SLURM_MEM_PER_NODE*90/100))
+
+
+autotar --sourcedir $SOURCEDIR --destdir $DESTDIR --group $GROUP -p $SLURM_NTASKS -m $MEMORY
