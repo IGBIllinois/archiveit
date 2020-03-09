1c82af62101968836b0fa3e621005169280018cf
David Slater <dslater@illinois.edu>
2020-03-09 10:59:42 -0500

    Uploaded path for storage for bin/archive_all_miseqs.sh

--- bin/archive_all_miseqs.sh
+++ bin/archive_all_miseqs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-YEAR=2017
+YEAR=2019
 DAYS=180
 LOCKFILE=/var/run/allmiseq
 
@@ -8,8 +8,8 @@ if [ -f $LOCKFILE ]; then
   echo $LOCKFILE already exists
 else
   touch $LOCKFILE 
-  miseq_archive.pl -dir=/storage/MiSeq/MiSeq_1/ -dest=/archive/CBC/MiSeq/$YEAR/ -exclude miseq -user aghernan -group biotech-develope -days $DAYS
-  miseq_archive.pl -dir=/storage/MiSeq/MiSeq_2/ -dest=/archive/CBC/MiSeq/$YEAR/ -exclude miseq -user aghernan -group biotech-develope -days $DAYS
-  miseq_archive.pl -dir=/storage/MiSeq/MiSeq_3/ -dest=/archive/CBC/MiSeq/$YEAR/ -exclude miseq -user aghernan -group biotech-develope -days $DAYS
+  miseq_archive.pl -dir=/storage3/MiSeq/MiSeq_1/ -dest=/archive/CBC/MiSeq/$YEAR/ -exclude miseq -user aghernan -group biotech-develope -days $DAYS
+  miseq_archive.pl -dir=/storage3/MiSeq/MiSeq_2/ -dest=/archive/CBC/MiSeq/$YEAR/ -exclude miseq -user aghernan -group biotech-develope -days $DAYS
+  miseq_archive.pl -dir=/storage3/MiSeq/MiSeq_3/ -dest=/archive/CBC/MiSeq/$YEAR/ -exclude miseq -user aghernan -group biotech-develope -days $DAYS
   rm $LOCKFILE
 fi
